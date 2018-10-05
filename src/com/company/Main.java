a73d3bfb4614d761afa6d4beb0057a4caf1b6c56
swimmerlap <dizzgil@aol.com>
2018-10-05 12:16:14 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int count = 35;
+        while (count >= 1) {
+            if (count % 7 == 0) {
+                System.out.println(count);
+            }
+            count--;
+        }
+
+
+
+        int countt = 40;
+        while (countt >= 1) {
+            if (countt % 5 == 0) {
+                System.out.println(countt);
+            }
+            countt--;
+        }
+    }
+
+}
+
+
+
+
